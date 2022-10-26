3bef4ef89b47ff6ad2b0fedcfd69bd709d8e15c8
jgmonteiro1 <jgmoreira15@gmail.com>
2022-10-26 01:55:18 -0300

    Ajustes no build gradle e no Dockerfile para ter conseguir rodar um container

--- src/main/java/com/jgmonteiro/springaws/controller/TestController.java
+++ src/main/java/com/jgmonteiro/springaws/controller/TestController.java
@@ -1,7 +1,5 @@
 package com.jgmonteiro.springaws.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/test")
 public class TestController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("/dog/{name}")
     public ResponseEntity<?> test(@PathVariable String name) {
-        LOG.info("Test controller - name:{}", name);
-
         return ResponseEntity.ok("Name: " + name);
     }
 }
